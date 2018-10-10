d576c8e731732740936fa38ce8b4983aa24d809e
Clauter Carlos <clauter.carlos@gmail.com>
2018-10-09 21:53:42 -0300

    Create shopping cart and menu item components to restaurant detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { RestaurantComponent } from './restaurants/restaurant/restaurant.compone
 import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 
 
 @NgModule({
@@ -25,7 +27,9 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
-    MenuComponent
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
