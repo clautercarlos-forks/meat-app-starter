5d2166781e22c743eb354ffb4b2ea6b8993603a0
Clauter Carlos <clauter.carlos@gmail.com>
2019-09-07 13:31:58 -0300

    Create a shalder dir with input component because the component will be used in many parts
    Signed-off-by: Clauter Carlos <clauter.carlos@gmail.com>

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "mt-input",
+  templateUrl: "./input.component.html"
+})
+export class InputComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
